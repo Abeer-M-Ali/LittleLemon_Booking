5999b0b055ca755ec0b7cf6a2ecb48e43d10f724
Abeer-M-Ali <abeerragab5211@gmail.com>
2024-02-27 02:44:43 +0200

    Final Submit before Submission

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import './App.css';
-import Nav from "./components/Nav"
-import Header from "./components/Header"
-import Main from "./components/Main"
-import Footer from "./components/Footer"
 
 function App() {
   return (
+ 
     <div>
-    <React.Fragment>
-      <Nav />
-      <Main />
-      <Footer />
-    </React.Fragment>
+      <h1>Meta Frontend Developer Capstone Starter Files</h1>
     </div>
   );
 }
